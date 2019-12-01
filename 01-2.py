a7aa83ad39f6016d1e580bca4fe48a1b507017b3
kroberts <cppwriter@yahoo.com>
2019-12-01 14:23:35 -0800

    cleaned up the code a bit

--- 01-2.py
+++ 01-2.py
@@ -1,25 +1,12 @@
 
-masses = []
 s = 0
 with open('01.txt') as f:
     for line in f:
         num = int(line)
         mass = (num // 3) - 2
-        masses.append(mass)
-        s += mass
+        while mass > 0:
+            s += mass
+            mass = (mass // 3) - 2
         pass
-
-while True:
-    nonzero = 0
-    nmasses = []
-    for m in masses:
-        nm = (m // 3) - 2
-        if nm > 0:
-            s += nm
-            nmasses.append(nm)
-            nonzero += 1
-    masses = nmasses
-    if nonzero == 0:
-        break
 
 print(s)
