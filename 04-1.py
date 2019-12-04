c91cd914cad92763d3d033b4a56fe5ce7acd6e25
kroberts <cppwriter@yahoo.com>
2019-12-03 21:11:19 -0800

    solved day 4

---
+++ 04-1.py
@@ -0,0 +1,42 @@
+import re
+import sys
+import json
+from collections import defaultdict
+sys.setrecursionlimit(5000)
+
+start = 382345
+stop  = 843167
+
+def proc(x):
+    digs = [int(d) for d in str(x)]
+    min = digs[0]
+    for i in digs[1:]:
+        if i < min:
+            return False
+        min = i
+
+    counts = defaultdict(lambda:0)
+    for i in digs:
+        counts[i] += 1
+
+    num2s = 0
+    for c in counts.values():
+        if c >= 2: num2s += 1
+
+    if num2s == 0:
+        return False
+
+    return True
+
+print(proc(111111))
+
+npass = 0
+for x in range(start, stop + 1):
+    if proc(x):
+        npass += 1
+
+print(npass)
+
+#with open('04.txt') as f:
+ #   lines = [x.strip() for x in f.readlines()]
+
