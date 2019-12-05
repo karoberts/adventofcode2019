dafc079c13dd40150561321aebba6ee7266a3373
kroberts <cppwriter@yahoo.com>
2019-12-04 21:38:19 -0800

    cleanup (since this will likely be seen again)

--- 05-2.py
+++ 05-2.py
@@ -21,64 +21,63 @@ with open('05.txt') as f:
     inp = [5]
     ip = 0
 
+    ops_size = {1:4, 2:4, 3:2, 4:2, 5:3, 6:3, 7:4, 8:4, 99:0}
     while True:
         opcode = ops[p] % 100
+        inc_p = True
         if opcode == 1:
             v1 = readv(ops, p, 1)
             v2 = readv(ops, p, 2)
             ops[ops[p+3]] = v1 + v2
             if output: print('add', v1, v2, ops[p+3])
-            p += 4
             pass
         elif opcode == 2:
             v1 = readv(ops, p, 1)
             v2 = readv(ops, p, 2)
             ops[ops[p+3]] = v1 * v2
             if output: print('mul', v1, v2, ops[p+3])
-            p += 4
             pass
         elif opcode == 3:
             v = inp[ip]
             ip += 1
             ops[ops[p+1]] = v
             if output: print('inp', ops[p+1], v)
-            p += 2
             pass
         elif opcode == 4:
             print('output:', readv(ops, p, 1))
-            p += 2
             pass
         elif opcode == 5:
             v1 = readv(ops, p, 1)
             if v1 != 0:
                 p = readv(ops, p, 2)
                 if output: print('jumpnz', p)
+                inc_p = False
             else:
                 if output: print('nojumpnz', v1)
-                p += 3
             pass
         elif opcode == 6:
             if readv(ops, p, 1) == 0:
                 p = readv(ops, p, 2)
                 if output: print('jumpz', p)
+                inc_p = False
             else:
                 if output: print('nojumpz')
-                p += 3
             pass
         elif opcode == 7:
             v1 = readv(ops, p, 1)
             v2 = readv(ops, p, 2)
             ops[ops[p+3]] = 1 if v1 < v2 else 0
             if output: print('lt', v1, v2, ops[p+3])
-            p += 4
             pass
         elif opcode == 8:
             v1 = readv(ops, p, 1)
             v2 = readv(ops, p, 2)
             ops[ops[p+3]] = 1 if v1 == v2 else 0
             if output: print('eq', v1, v2, ops[p+3])
-            p += 4
             pass
         elif opcode == 99:
             if output: print('HALT')
             break
+
+        if inc_p:
+            p += ops_size[opcode]
