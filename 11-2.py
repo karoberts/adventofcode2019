ad137038b1676476d71c3caa948396de2e512712
kroberts <cppwriter@yahoo.com>
2019-12-10 21:24:06 -0800

    solved 11 - 2

---
+++ 11-2.py
@@ -0,0 +1,146 @@
+import re
+import sys
+import json
+import itertools
+from collections import defaultdict
+sys.setrecursionlimit(5000)
+
+#ctx {'p': 0, 'pi':0, 'rb':0 'i':[i]})
+def run_prog(ops, ctx, inp):
+    modes = {0: 'POS', 1:'IMM', 2:'REL'}
+
+    def readv(ops, p, offs, rb):
+        if offs == 1: mode = modes[(ops[p] % 1000) // 100]
+        elif offs == 2: mode = modes[(ops[p] % 10000) // 1000]
+        elif offs == 3: mode = modes[(ops[p] % 100000) // 10000]
+        #print('r', p, mode, ops[p], ops[p + offs])
+        if mode == 'IMM': return ops[p + offs]
+        elif mode == 'POS': return ops[ops[p + offs]]
+        elif mode == 'REL': return ops[ops[p + offs] + rb]
+
+    def writev(ops, p, offs, rb, v):
+        if offs == 1: mode = modes[(ops[p] % 1000) // 100]
+        elif offs == 2: mode = modes[(ops[p] % 10000) // 1000]
+        elif offs == 3: mode = modes[(ops[p] % 100000) // 10000]
+        #print('r', p, mode, ops[p], ops[p + offs])
+        if mode == 'POS': ops[ops[p + offs]] = v
+        elif mode == 'REL': ops[ops[p + offs] + rb] = v
+
+    ops_size = {1:4, 2:4, 3:2, 4:2, 5:3, 6:3, 7:4, 8:4, 9:2, 99:0}
+    output = False
+    while True:
+        opcode = ops[ctx['p']] % 100
+        inc_p = True
+        if opcode == 1:
+            v1 = readv(ops, ctx['p'], 1, ctx['rb'])
+            v2 = readv(ops, ctx['p'], 2, ctx['rb'])
+            writev(ops, ctx['p'], 3, ctx['rb'], v1 + v2)
+            if output: print('add', v1, v2, ops[ctx['p']+3])
+            pass
+        elif opcode == 2:
+            v1 = readv(ops, ctx['p'], 1, ctx['rb'])
+            v2 = readv(ops, ctx['p'], 2, ctx['rb'])
+            writev(ops, ctx['p'], 3, ctx['rb'], v1 * v2)
+            if output: print('mul', v1, v2, ops[ctx['p']+3])
+            pass
+        elif opcode == 3:
+            v = inp[ctx['pi']]
+            ctx['pi'] += 1
+            writev(ops, ctx['p'], 1, ctx['rb'], v)
+            if output: print('inp', ops[ctx['p']+1], v)
+            pass
+        elif opcode == 4:
+            v1 = readv(ops, ctx['p'], 1, ctx['rb'])
+            if output: print('output:', v1)
+            ctx['p'] += ops_size[opcode]
+            return v1
+            pass
+        elif opcode == 5:
+            v1 = readv(ops, ctx['p'], 1, ctx['rb'])
+            if v1 != 0:
+                ctx['p'] = readv(ops, ctx['p'], 2, ctx['rb'])
+                if output: print('jumpnz', ctx['p'])
+                inc_p = False
+            else:
+                if output: print('nojumpnz', v1)
+            pass
+        elif opcode == 6:
+            if readv(ops, ctx['p'], 1, ctx['rb']) == 0:
+                ctx['p'] = readv(ops, ctx['p'], 2, ctx['rb'])
+                if output: print('jumpz', ctx['p'])
+                inc_p = False
+            else:
+                if output: print('nojumpz')
+            pass
+        elif opcode == 7:
+            v1 = readv(ops, ctx['p'], 1, ctx['rb'])
+            v2 = readv(ops, ctx['p'], 2, ctx['rb'])
+            writev(ops, ctx['p'], 3, ctx['rb'], 1 if v1 < v2 else 0)
+            if output: print('lt', v1, v2, ops[ctx['p']+3])
+            pass
+        elif opcode == 8:
+            v1 = readv(ops, ctx['p'], 1, ctx['rb'])
+            v2 = readv(ops, ctx['p'], 2, ctx['rb'])
+            writev(ops, ctx['p'], 3, ctx['rb'], 1 if v1 == v2 else 0)
+            if output: print('eq', v1, v2, ops[ctx['p']+3])
+            pass
+        elif opcode == 9:
+            v1 = readv(ops, ctx['p'], 1, ctx['rb'])
+            ctx['rb'] += v1
+            if output: print('relbase', v1)
+            pass
+        elif opcode == 99:
+            if output: print('HALT')
+            return None
+
+        if inc_p:
+            ctx['p'] += ops_size[opcode]
+
+
+with open('11.txt') as f:
+    ops = defaultdict(lambda:0)
+    i = 0
+    for x in f.readline().strip().split(','):
+        ops[i] = int(x)
+        i += 1
+
+    grid = defaultdict(lambda:0)
+    p = (0,0)
+    grid[p] = 1
+    face = (0, -1)
+    inp = [1]
+    ctx =  {'p': 0, 'pi':0, 'rb':0}
+
+    left = { (0,-1): (-1,0),  (-1,0): (0,1),  (0,1): (1,0),  (1,0): (0,-1) }
+    right = { (0,-1): (1,0),   (1,0): (0,1),  (0,1): (-1,0),  (-1,0): (0,-1) }
+
+    painted = set()
+    _min = [0,0]
+    _max = [0,0]
+    while True:
+        color = run_prog(ops, ctx, inp)
+        #print('color', color)
+        if color is None: break
+        direc = run_prog(ops, ctx, inp)
+        #print('direc', direc)
+        if direc is None: break
+
+        painted.add(p) 
+        grid[p] = color
+        if direc == 0: face = left[face]
+        else: face = right[face]
+
+        p = (p[0] + face[0], p[1] + face[1])
+        if p[0] > _max[0]: _max[0] = p[0]
+        if p[1] > _max[1]: _max[1] = p[1]
+        if p[0] < _min[0]: _min[0] = p[0]
+        if p[1] < _min[1]: _min[1] = p[1]
+        inp.append(grid[p])
+        #print('p', p)
+
+
+    for y in range(_min[1], _max[1] + 1):
+        for x in range(_min[0], _max[0] + 1):
+            if grid[(x,y)] == 1: print ('*', end='')
+            else: print(' ', end='')
+        print()
