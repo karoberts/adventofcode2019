55d2f22df351c3770d9b1229d15e9f8de3abd4f9
Keith Roberts <keith@ntersol.com>
2019-12-12 17:23:06 -0800

    still working on 12-2

--- 12-2.py
+++ 12-2.py
@@ -13,15 +13,20 @@ with open('12.txt') as f:
         data = [a.split('=') for a in coords]
         moons[i] = {'p': [int(data[0][1]), int(data[1][1]), int(data[2][1])], 'v':[0,0,0]}
         i += 1
-    print(moons)
 
     states = dict()
+    states[str(moons)] = (moons, 0)
 
+    print(moons)
+
+    xs = [moons[0]['p'][0]]
+    
     step = 0
     while True:
 
-        if step % 10000 == 0:
+        if step > 0 and step % 10000 == 0:
             print(step)
+            break
 
         for mk1 in range(0, len(moons)):
             for mk2 in range(mk1 + 1, len(moons)):
@@ -42,12 +47,23 @@ with open('12.txt') as f:
         for m in moons.keys():
             for i in range(0,3):
                 moons[m]['p'][i] += moons[m]['v'][i]
+            
+        #print(step, moons[0]['v'][0])
 
-        hash = str(moons)
-        if hash in states:
-            break
-        states[hash] = moons
+        xs.append(moons[0]['p'][0])
 
         step += 1
 
+        hash = str(moons)
+        if hash in states:
+            print('dupe', states[hash][1], moons)
+            break
+        states[hash] = (moons, step)
+
+
+    #print(moons)
     print('part2', step)
+
+    for i in range(1,len(xs) // 2):
+        if xs[:i] == xs[i:i*2]:
+            print('cycle at', i)
