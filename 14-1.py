5c3b777cd8bd633097e62e20a724381a74651624
kroberts <cppwriter@yahoo.com>
2019-12-13 22:16:31 -0800

    working on 14-1

--- 14-1.py
+++ 14-1.py
@@ -24,24 +24,54 @@ with open('14-test1.txt') as f:
     #print(prod['FUEL'])
 
     q = []
-    q.append(('FUEL', 1))
-    have = defaultdict(lambda:0)
+    q.append('FUEL')
+    extras = defaultdict(lambda:0)
     chneed = defaultdict(lambda:0)
+    chneed['FUEL'] = 1
+    ore = 0
 
     while len(q) > 0:
-        n = q.pop(0)
+        ch = q.pop(0)
+        n = (ch, chneed[ch])
         print('trying to make', n[1], n[0])
 
-        if n[0] == 'ORE':
-            continue
-
         for need in prod[n[0]]['i']:
-            ineed = need[1] * n[1]
-            canmake = prod[need[0]]['c'] if need[0] != 'ORE' else ineed
-            multiple = ineed // canmake if ineed % canmake == 0 else ineed // canmake + 1
+            if chneed[n[0]] == 0:
+                continue
 
-            q.append((need[0], need[1] * multiple))
-            chneed[need[0]] += need[1] * multiple
-            have[need[0]] += need[1] * multiple
+            if need[0] == 'ORE':
+                if n[1] % prod[n[0]]['c'] == 0:
+                    needore = (n[1] // prod[n[0]]['c']) * need[1]
+                elif n[1] < prod[n[0]]['c']:
+                    needore = need[1]
+                else:
+                    needore = ((n[1] // prod[n[0]]['c']) + 1) * need[1]
+                print('  need ORE', needore, 'd', n[1], need[1], prod[n[0]]['c'])
+                ore += needore
+                chneed[n[0]] = 0
+                continue
+
+            ineed = need[1] * n[1]
+            canmake = prod[need[0]]['c']
+            if ineed % canmake == 0:
+                multiple = ineed
+            elif ineed < canmake:
+                multiple = canmake
+                print('    making', multiple - ineed, 'extra', need[0])
+                extras[need[0]] += (multiple - ineed)
+            else:
+                multiple = ((ineed // canmake) + 1) * canmake
+                while extras[need[0]] > 0 and multiple % ineed != 0:
+                    print('    using an extra', need[0])
+                    multiple -= 1
+                    extras[need[0]] -= 1
+                print('    making', multiple - ineed, 'extra', need[0])
+                extras[need[0]] += (multiple - ineed)
+
+            print('  need to make', multiple, need[0])
+            q.append(need[0])
+            chneed[need[0]] += multiple
 
     print(chneed)
+    print(extras)
+    print(ore)
