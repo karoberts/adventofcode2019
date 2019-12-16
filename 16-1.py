74fafc4b1245d21c255d8b00b3459f4b99306dc4
kroberts <cppwriter@yahoo.com>
2019-12-15 21:24:38 -0800

    solved 16-1

---
+++ 16-1.py
@@ -0,0 +1,40 @@
+import re
+import sys
+import json
+import itertools
+from collections import defaultdict
+sys.setrecursionlimit(5000)
+
+def make_base(pos:int, dlen:int):
+    basesig = [0, 1, 0, -1]
+    bpos = 0
+    ngen = 0
+    while ngen < dlen + 1:
+        for j in range(0, pos + 1):
+            if ngen == 0 and j == 0:
+                ngen += 1
+                continue 
+            yield basesig[bpos]
+            ngen += 1
+        bpos += 1
+        if bpos == 4: bpos = 0
+
+
+with open('16.txt') as f:
+    digits = [int(x) for x in f.readline().strip()]
+    #digits = [1,2,3,4,5,6,7,8]
+
+    odigits = [None] * len(digits)
+    for phase in range(0, 100):
+        for d in range(0, len(digits)):
+            val = 0
+            pat = list(make_base(d, len(digits)))
+            for i in range(0, len(digits)):
+                val += pat[i] * digits[i]
+            val = abs(val) % 10
+            odigits[d] = val
+        digits = odigits
+        #print(phase + 1)
+        #print(phase, odigits)
+
+    print(''.join((str(x) for x in odigits[0:8])))
