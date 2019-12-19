f0fba6719c4b8bc49d3b10ac404c62143359f048
kroberts <cppwriter@yahoo.com>
2019-12-19 07:59:54 -0800

    solved 19-2

--- 19-2.py
+++ 19-2.py
@@ -115,23 +115,76 @@ with open('19.txt') as f:
         ops[i] = int(x)
         i += 1
 
+    _min = [0,0]
+    _max = [1600,1600]
+
+    x_start = dict()
+    x_stop = dict()
+    y_start = dict()
+    y_stop = dict()
+
     grid = defaultdict(lambda:' ')
     deadends = set()
-    _min = [0,0]
-    _max = [50,50]
-    beamzones = 0
     for y in range(_min[1], _max[1]):
-        for x in range(_min[0], _max[0]):
+        xst = x_start[y-1] if (y-1) in x_start else 0
+        for x in range(xst, _max[0]):
             inp = [x,y]
             ctx =  {'p': 0, 'pi':0, 'rb':0}
             r = run_prog(ops.copy(), ctx, inp)
             #print(x,y,r)
             if r == 0:
                 grid[(x,y)] = '.'
+                if y in x_start:
+                    break
             elif r == 1:
                 grid[(x,y)] = '#'
-                beamzones += 1
+                if x not in y_start:
+                    y_start[x] = y
+                if y not in x_start:
+                    x_start[y] = x
+                y_stop[x] = y
+                x_stop[y] = x
+                """
+                if x - x_start[y] == 100:
+                    grid[(x_start[y],y)] = 'O'
+                    grid[(x-1,y)] = 'Z'
+                    if y - y_start[x-1] == 100:
+                        grid[(x-1, y_start[x-1])] = '!'
+                        grid[(x_start[y], y_start[x-1])] = 'A'
+                        print('found', (x-1,y), (x-1,y_start[x-1]), 'answer?', (x_start[y], y_start[x-1]), (x_start[y] * 10000 + y_start[x-1]), file=sys.stderr)
+                """
+        if y > 400:
+            low_startx = x_start[y]
+            low_endx = x_stop[y]
+            high_startx = low_startx
+            high_starty = y - 99
+            high_endx = x_stop[y - 99]
+            if low_endx - low_startx >= 99 and high_endx - high_startx >= 99:
+                print(low_startx, low_endx, high_startx, high_starty, high_endx, 'answer', high_startx, high_starty, high_startx * 10000 + high_starty)
+                exit()
 
-    printg(grid, _min, _max)
+    """
+    stops = []
+    for k, v in x_stop.items():
+        y = k
+        ys = x_start[y]
+        x = v
+        s = (x, y, v - x_start[y], y_stop[x] - y_start[x])
+        stops.append(s)
+        if s[2] >= 5 and s[3] >= 5:
+            xs = x_start[ys]
+            ys = y_start[xs]
+            grid[(xs, ys)] = 'O'
+            print(s, xs, ys)
+    """
 
-    print('part1', beamzones)
+    #printg(grid, _min, _max)
+
+"""
+    for y in range(_min[1], _max[1]):
+        if y in x_start and x_stop[y] - x_start[y] >= 5:
+            for x in range(x_start[y], x_stop[y]):
+                if x in y_start and y_stop[x] - y_start[x] >= 5:
+                    grid[(x,y)] = 'O'
+                    """
+
